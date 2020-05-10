5c09d1c29f3010a0acfd2f40542c46ccbdd3a33c
Francisco A. Tapias Bravo <francisco_tapias@external.mckinsey.com>
2020-05-10 18:18:30 +0200

    parsea, limpia y transforma datos de peso y medida a formato uniforme. Ya calcula IMC, clasifica IMC y corrige autoestima Rossemberg

--- corrector_masivo.py
+++ corrector_masivo.py
@@ -130,27 +130,82 @@ bsq_cooper = [
 # carga file
 cuestionario = pandas.read_csv('../cuestionario.csv')
 
-resultados = {"Edad": [],
+resultados = {"Id":[],
+              "Edad": [],
               "Género": [],
               "Peso": [],
               "Altura": [],
+              "IMC": [],
+              "Clasificación IMC": [],
               "Autoestima Rossemberg": [],
-              "Alexitimia TAS-20", [],
+              "Valoración Rossemberg": [],
+              "Alexitimia TAS-20": [],
               "BSQ Cooper": []
               }
 
 
-for i in range((cuestionario.shape[0])):      
+def parse_to_numeric(cadena):
+    convertido = ""
+    for n in cadena:
+        if n.isdigit():
+            convertido = convertido + n
+        else:
+            if n == "/":
+                partes = cadena.split("/")
+                total = ((float(partes[1]) - float(partes[0]))/2) + float(partes[0])
+                convertido = str(total)
+                break
+            if n == ",":
+                convertido = convertido + n.replace(",",".")
+            if n == "'":
+                convertido = convertido + n.replace("'",".")
+            if n == ".":
+                convertido = convertido + n  
+    return convertido
+
+
+for i in range((cuestionario.shape[0])):
     row = list(cuestionario.iloc[i, :])
+    resultados["Id"].append(row[0])
+    resultados["Edad"].append(parse_to_numeric(row[7]))
+    resultados["Género"].append(row[8])
+    peso = float(parse_to_numeric(row[9]))
+    # print("peso: %s %s" % (row[9],peso))
+    raw_altura = parse_to_numeric(row[10])
+    if raw_altura.find(".") != -1:
+        num = float(raw_altura) * 100
+        raw_altura = f'{num:.2f}'
+    altura = float(raw_altura)
+    # print("altura: %s %s " % (row[10],altura))
+    resultados["Peso"].append(int(peso))
+    resultados["Altura"].append(int(altura))
+    imc = peso / (pow(altura / 100, 2))
+    resultados["IMC"].append(f'{imc:.2f}')
+    # clasificacion IMC
+    if imc < 18.5:
+        resultados["Clasificación IMC"].append("Bajo peso")
+    if 18.5 <= imc and imc < 25:
+        resultados["Clasificación IMC"].append("Normopeso")
+    if 25 <= imc and imc < 30:
+        resultados["Clasificación IMC"].append("Sobrepeso")
+    if 30 < imc and imc < 35:
+        resultados["Clasificación IMC"].append("Obesidad tipo I")
+    if 35 < imc and imc < 40:
+        resultados["Clasificación IMC"].append("Obesidad tipo II")
+    if 40 <= imc:
+        resultados["Clasificación IMC"].append("Mórbida")
 
     # test rossemberg
+    score = 0
     for test in autoestima_rossemberg:
-      resultado=0
-      for item in test["items"]:
-        # pprint(row[item+4])
-        resultado = resultado + test["puntuaciones"][row[item+4].replace(".","")]
-      print("%s %s" % (test["group"], resultado))
-    break
+        for item in test["items"]:
+            # pprint(row[item+4])
+            score = score + test["puntuaciones"][row[item+4].replace(".","")]
+        # print("%s %s" % (test["group"], score))
+    resultados["Autoestima Rossemberg"].append(score)
+    
     # break
+
     # row_list.append(list(cuestionario.iloc[i, :])) 
+pprint(resultados)
 
